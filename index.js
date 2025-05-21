d0ec7bb52e07b40374d6b0136210e91b84aed16c
Farrelio <imcominfordoom@gmail.com>
2025-05-21 08:51:21 +0700

    Refactor Firebase initialization to use environment variables and improve code structure

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ dotenv.config();
 const express = require('express');
 const mqtt = require('mqtt');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccount.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSAL_DOMAIN,
+  }),
+});
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
 
 const db = admin.firestore();
 
@@ -39,7 +51,8 @@ mqttClient.on('message', async (topic, message) => {
             ph: ph,
             moisture: moisture,
             tds: tds,
-            timeStamp: admin.firestore.FieldValue.serverTimestamp()
+            timeStamp: admin.firestore.FieldValue.
+            serverTimestamp()
         });
 
         console.log('Data successfully written to Firestore!');
